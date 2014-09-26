7ad6d473667d0ec78023c7b8267bfe316bb9b779
adityakalushte <aditya_kalushte@rediffmail.com>
2014-09-26 23:46:33 +0530

    new frequency reuse added

---
+++ MCProj/src/MCProj/Main.java
@@ -0,0 +1,38 @@
+package MCProj;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Started");
+		
+		SwingUtilities.invokeLater(new Runnable()
+                {
+                    
+                @Override
+                public void run()
+                {
+                    Simulator x=new Simulator();
+                    Contents content=new Contents();
+
+                    JPanel app=new JPanel();
+                    app.setLayout(new BoxLayout(app, BoxLayout.Y_AXIS));
+                    app.add(content);
+
+                    x.add(app);
+                    x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    x.setSize(400,400);
+                    x.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                    x.setVisible(true);
+                }
+        });	
+				
+		System.out.println("Done");
+	}
+}
